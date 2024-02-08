f829d8732084e848ef797376cc7c113256b13c09
マリウス <marius@xn--gckvb8fzb.com>
2024-02-08 07:53:01 -0500

    Moved Unicorn code into dedicated output

--- lemon.go
+++ lemon.go
@@ -2,59 +2,16 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
 	"log"
 	"os"
 	"strings"
-
-	"periph.io/x/conn/v3/spi"
-	"periph.io/x/conn/v3/spi/spireg"
-	"periph.io/x/devices/v3/unicornhd"
-	"periph.io/x/host/v3"
 
 	"github.com/mrusme/lemon/inbox"
 	"github.com/mrusme/lemon/output"
 	"github.com/mrusme/lemon/pushover"
 )
 
-var port spi.PortCloser
-
-func setup() {
-	var err error
-
-	if _, err = host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	port, err = spireg.Open("")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	uchd, err := unicornhd.New(port)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(uchd.String())
-
-	uchd.Draw(uchd.Bounds(), &image.Uniform{color.RGBA{0, 0, 0, 0}}, image.Point{})
-
-	fmt.Println("Ok")
-}
-
-func bye() {
-
-	if err := port.Close(); err != nil {
-		log.Fatal(err)
-	}
-
-}
-
 func main() {
-	dry := os.Getenv("DRY_LEMON")
 	deviceId := os.Getenv("PUSHOVER_DEVICE_ID")
 	secret := os.Getenv("PUSHOVER_SECRET")
 	outputsString := os.Getenv("LEMON_OUTPUTS")
@@ -66,12 +23,6 @@ func main() {
 			panic(err)
 		}
 		outputs = append(outputs, o)
-	}
-
-	fmt.Println(dry)
-	if dry == "" {
-		defer bye()
-		setup()
 	}
 
 	ibx := make(chan inbox.Message)
--- output/unicorn/unicorn.go
+++ output/unicorn/unicorn.go
@@ -1,16 +1,55 @@
 package unicorn
 
-import "github.com/mrusme/lemon/inbox"
+import (
+	"image"
+	"image/color"
+	"time"
 
-type Unicorn struct{}
+	"github.com/mrusme/lemon/inbox"
+	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
+	"periph.io/x/devices/v3/unicornhd"
+	"periph.io/x/host/v3"
+)
+
+type Unicorn struct {
+	port spi.PortCloser
+	uchd *unicornhd.Dev
+}
 
 func (out *Unicorn) Setup() error {
+	var err error
+
+	if _, err = host.Init(); err != nil {
+		return err
+	}
+
+	out.port, err = spireg.Open("")
+	if err != nil {
+		return err
+	}
+
+	out.uchd, err = unicornhd.New(out.port)
+	if err != nil {
+		return err
+	}
+
+	out.uchd.Draw(out.uchd.Bounds(), &image.Uniform{color.RGBA{0, 0, 0, 0}}, image.Point{})
+
 	return nil
 }
 
 func (out *Unicorn) Cleanup() {
+	if err := out.port.Close(); err != nil {
+	}
 }
 
 func (out *Unicorn) Display(ibxMsg *inbox.Message) error {
+	p := image.Pt(0, 0)
+	for i := 0; i < out.uchd.Bounds().Dx(); i++ {
+		p.X = -1 * i
+		out.uchd.Draw(out.uchd.Bounds(), ibxMsg.Icon, ibxMsg.Icon.Bounds().Bounds().Min.Add(p))
+		time.Sleep(time.Millisecond * 100)
+	}
 	return nil
 }
